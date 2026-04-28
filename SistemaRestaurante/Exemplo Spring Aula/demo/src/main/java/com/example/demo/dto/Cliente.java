982c8cb73a7552e78140fd8d4544a25257806dee
Gabriel Santos de Lima <gabrielsantoscr97@gmail.com>
2026-04-28 19:25:48 -0300

    desenvolvendo clienteDTO

---
+++ SistemaRestaurante/Exemplo Spring Aula/demo/src/main/java/com/example/demo/dto/Cliente.java
@@ -0,0 +1,26 @@
+package com.example.demo.dto;
+
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+public class ClienteDTO {
+    
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    private String nome;
+
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "O e-mail é obrigatório")
+    private String email;
+
+    @NotBlank(message = "O telefone é obrigatório")
+    private String telefone;
+}
