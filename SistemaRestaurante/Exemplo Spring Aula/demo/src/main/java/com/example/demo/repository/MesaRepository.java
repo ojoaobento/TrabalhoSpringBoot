dcb1ea1a0c064a739d3df9e417e580a67d92ad47
João Bento <joaovictorbento.s.b@gmail.com>
2026-04-29 13:41:58 -0300

    Atualizando estruturas de Mesa

--- SistemaRestaurante/Exemplo Spring Aula/demo/src/main/java/com/example/demo/repository/MesaRepository.java
+++ SistemaRestaurante/Exemplo Spring Aula/demo/src/main/java/com/example/demo/repository/MesaRepository.java
@@ -11,8 +11,5 @@ import java.util.List;
 
 @Repository
 public interface MesaRepository extends JpaRepository<Mesa, Long> {
-    Optional<Mesa> findByNumero(Integer numero);
-
     List<Mesa> findByStatusIgnoreCase(String status);
-
 }
